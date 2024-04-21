133c66f176bedf4f90ec22db804843fda0666892
Kevin Motia <kevindmotia@gmail.com>
2024-04-21 13:15:12 -0400

    update

--- dataCleaner.py
+++ dataCleaner.py
@@ -1,26 +1,25 @@
 import pandas as pd
 
-
+# Read in raw year-month CSVs
 BCE_df = pd.read_csv('BCEMAN_ts.csv')
 OCE_df = pd.read_csv('OCEMAN_ts.csv')
 SO2E_df = pd.read_csv('SO2EMAN_ts.csv')
 SO4E_df = pd.read_csv('SO4EMAN_ts.csv')
 
-raw_profit_df = pd.read_csv('raw_profit_ts.csv')
-
-
+# Function to turn year-month into a date
 def get_pollutants_dates(df):
     df['Date'] = pd.to_datetime(df[['Year', 'Month']].assign(day=1))
     df = df.drop(columns=['Year', 'Month'])
     df = df[['Date'] + [col for col in df.columns if col != 'Date']]
     return df
 
-
+# Turn year-month into dates
 BCE_df = get_pollutants_dates(BCE_df)
 OCE_df = get_pollutants_dates(OCE_df)
 SO2E_df = get_pollutants_dates(SO2E_df)
 SO4E_df = get_pollutants_dates(SO4E_df)
 
+# Output CSVs with dates
 BCE_df.to_csv('clean_BCEMAN_ts.csv', index=False)
 OCE_df.to_csv('clean_OCEMAN_ts.csv', index=False)
 SO2E_df.to_csv('clean_SO2EMAN_ts.csv', index=False)
@@ -34,8 +33,7 @@ SO4E_df.to_csv('clean_SO4EMAN_ts.csv', index=False)
 # SO4E_df = pd.read_csv('clean_SO4EMAN_ts.csv')
 
 
-print(SO2E_df)
-
+# Function to aggregate monthly data to quarterly data
 def aggregate_months(df):
     # Group by year and calculate the sum for every 3 consecutive months
     df['Year'] = df['Date'].dt.year
@@ -56,10 +54,9 @@ def aggregate_months(df):
 
     return df
 
-
+# Aggregate monthly data to quarterly data
 BCE_df = aggregate_months(BCE_df)
 OCE_df = aggregate_months(OCE_df)
 SO2E_df = aggregate_months(SO2E_df)
 SO4E_df = aggregate_months(SO4E_df)
 
-print(SO2E_df)
