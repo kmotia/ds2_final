5130f8de05fc59edb39327f7087ac19e37e0ad49
Kevin Motia <kevindmotia@gmail.com>
2024-04-25 19:02:35 -0400

    Adjusting functions and datetime

--- dataCleanerV2.py
+++ dataCleanerV2.py
@@ -72,29 +72,115 @@ plt.show()
 
 #################### Preprocess FRED data ####################
 def adjust_for_inflation(df):
-    # Ensure 'Date' column is set as datetime
-    df['Date'] = pd.to_datetime(df['Date'])
+    # Ensure 'Date' column is set as datetime index
+    # df['Date'] = pd.to_datetime(df['Date'])
 
-    # Determine the name of the second column
-    second_column = df.columns[1]
+    # Determine the name of the feature column
+    feature_column = df.columns[0]
 
-    # Adjust values in the second column for inflation
-    for i, row in df.iterrows():
-        year = row['Date'].year
-        adjusted_value = cpi.inflate(row[second_column], year)      # Adjusts for inflation using a benchmark year --> (Most recent completed year for this project is 2023)
-        df.at[i, second_column] = adjusted_value
+    # Adjust values in the feature column for inflation
+    for date, value in df.iterrows():
+        year = date.year
+        adjusted_value = cpi.inflate(value[feature_column], year, to=2023)
+        df.at[date, feature_column] = adjusted_value
+
     return df
 
-profit_df = pd.read_csv('raw_profit_ts.csv')
-profit_df.rename(columns={'DATE': 'Date'}, inplace=True)
-profit_df.rename(columns={'A053RC1Q027SBEA': 'Profit'}, inplace=True)
-# profit_df['Date'] = pd.to_datetime(profit_df['Date'])                     # Convert to datetime format
-profit_df = adjust_for_inflation(profit_df)
+def adjust_index_base_year(df):
+    # Filter data for the year 2023
+    # df['Date'] = pd.to_datetime(df['Date'])
+
+    df_2023 = df[df.index.year == 2023]
+
+    # Calculate average of "IP Index" values for the year 2023
+    avg_2023 = df_2023["IP Index"].mean()
+
+    # Calculate Conversion Factor
+    conversion_factor = 100 / avg_2023
+
+    # Multiply "IP Index" values by Conversion Factor
+    df["IP Index"] = df["IP Index"] * conversion_factor
+
+    return df
+
+# Example usage:
+# Assuming df is your pandas DataFrame and initial_num is the initial number
+# adjusted_df = adjust_index_base_year(df, initial_num)
 
 
 
-# Repeat for every FRED CSV file
 
+# Rename columns. Adjust index or adjust for inflation for appropriate features
+
+autoGas_df = pd.read_csv('raw_predictor_files/IP_autoGas.csv')
+autoGas_df.columns.values[0] = 'Date'
+autoGas_df.columns.values[1] = 'IP Index'
+autoGas_df['Date'] = pd.to_datetime(autoGas_df['Date'])
+autoGas_df.set_index('Date', inplace=True)
+autoGas_df = adjust_index_base_year(autoGas_df)                 # adjust index base year to 2023 = 100
+
+basicChem_df = pd.read_csv('raw_predictor_files/IP_basicChem.csv')
+basicChem_df.columns.values[0] = 'Date'
+basicChem_df.columns.values[1] = 'IP Index'
+basicChem_df['Date'] = pd.to_datetime(basicChem_df['Date'])
+basicChem_df.set_index('Date', inplace=True)
+basicChem_df = adjust_index_base_year(basicChem_df)              # adjust index base year to 2023 = 100
+
+fuelOil_df = pd.read_csv('raw_predictor_files/IP_fuelOil.csv')
+fuelOil_df.columns.values[0] = 'Date'
+fuelOil_df.columns.values[1] = 'IP Index'
+fuelOil_df['Date'] = pd.to_datetime(fuelOil_df['Date'])
+fuelOil_df.set_index('Date', inplace=True)
+fuelOil_df = adjust_index_base_year(fuelOil_df)                  # adjust index base year to 2023 = 100
+
+manufacturing_df = pd.read_csv('raw_predictor_files/IP_manufacturing.csv')
+manufacturing_df.columns.values[0] = 'Date'
+manufacturing_df.columns.values[1] = 'IP Index'
+manufacturing_df['Date'] = pd.to_datetime(manufacturing_df['Date'])
+manufacturing_df.set_index('Date', inplace=True)
+manufacturing_df = adjust_index_base_year(manufacturing_df)      # adjust index base year to 2023 = 100
+
+paveRoofPC_df = pd.read_csv('raw_predictor_files/IP_paveRoofPetrol.csv')
+paveRoofPC_df.columns.values[0] = 'Date'
+paveRoofPC_df.columns.values[1] = 'IP Index'
+paveRoofPC_df['Date'] = pd.to_datetime(paveRoofPC_df['Date'])
+paveRoofPC_df.set_index('Date', inplace=True)
+paveRoofPC_df = adjust_index_base_year(paveRoofPC_df)            # adjust index base year to 2023 = 100
+
+PC_df = pd.read_csv('raw_predictor_files/IP_petrolCoal.csv')
+PC_df.columns.values[0] = 'Date'
+PC_df.columns.values[1] = 'IP Index'
+PC_df['Date'] = pd.to_datetime(PC_df['Date'])
+PC_df.set_index('Date', inplace=True)
+PC_df = adjust_index_base_year(PC_df)                            # adjust index base year to 2023 = 100
+
+total_pop = pd.read_csv('raw_predictor_files/total_pop.csv')
+total_pop.columns.values[0] = 'Date'
+total_pop.columns.values[1] = 'IP Index'
+total_pop['Date'] = pd.to_datetime(total_pop['Date'])
+total_pop.set_index('Date', inplace=True)
+total_pop = adjust_index_base_year(total_pop)                    # adjust index base year to 2023 = 100
+
+profit_df = pd.read_csv('raw_predictor_files/profit_corporate.csv')
+profit_df.columns.values[0] = 'Date'
+profit_df.columns.values[1] = 'Profit'
+profit_df['Date'] = pd.to_datetime(profit_df['Date'])
+profit_df.set_index('Date', inplace=True)
+profit_df = adjust_for_inflation(profit_df)                         # adjust for inflation
+
+volatileEnergy_df = pd.read_csv('raw_predictor_files/volatility_energyEnv.csv')
+volatileEnergy_df.columns.values[0] = 'Date'
+volatileEnergy_df.columns.values[1] = 'Volatility Index'
+volatileEnergy_df['Date'] = pd.to_datetime(volatileEnergy_df['Date'])
+volatileEnergy_df.set_index('Date', inplace=True)
+volatileEnergy_df = adjust_for_inflation(volatileEnergy_df)         # adjust for inflation
+
+workingPop_df = pd.read_csv('raw_predictor_files/working_pop.csv')
+workingPop_df.columns.values[0] = 'Date'
+workingPop_df.columns.values[1] = 'Working Population'
+workingPop_df['Date'] = pd.to_datetime(workingPop_df['Date'])
+workingPop_df.set_index('Date', inplace=True)
+                                                                    # No adjustments necessary. Units are "number of people"
 
 
 
@@ -107,7 +193,7 @@ profit_df = adjust_for_inflation(profit_df)
 # Plot each pollutant as a function of time
 plt.figure(figsize=(10, 6))
 
-plt.plot(profit_df['Date'], profit_df['Profit'], label='National Corporate Profit')
+plt.plot(profit_df.index, profit_df['Profit'], label='National Corporate Profit')
 
 plt.xlabel('Time')
 plt.ylabel('Billions of Dollars')
